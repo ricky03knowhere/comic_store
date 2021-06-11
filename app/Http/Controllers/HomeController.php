3be48b588d99442af5e95f4dcfd2cee8017a8701
ricky03knowhere <goose73thecat@gmail.com>
2021-06-12 02:25:17 +0700

    fill customer dashboard data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+
+use Illuminate\Support\Facades\DB; 
+
+use App\Models\Order;
+use App\Models\Detail_order;
+
+
 class HomeController extends Controller
 {
     /**
@@ -20,9 +27,57 @@ class HomeController extends Controller
      * @return \Illuminate\View\View
      */
     public function index() {
+
+        $user = auth() ->user();
         
-        $dashboard =  (auth() ->user() ->id) ? 'admin/adminHome' : 'dashboard';
-        
-        return view($dashboard);
+        $dashboard =  (($user ->status) == 1) ? 'admin/home' : 'home';
+
+
+        return redirect($dashboard);
     }
+
+
+    public function customer() {
+        $user = auth() ->user();
+        $user_order = Order::where('user_id',  $user -> id) ->first();
+        
+        $order = $user_order ->where('status', '==', 0) ->first();
+        
+        $get_order = Detail_order::where('order_id', $order ->id) ->first();
+        $get_count = Detail_order::where('order_id', $order ->id) ->get();
+        
+        $cart = $get_count ->sum('quantity');
+        $total_pay = $get_order -> order ->total_price;
+
+
+        $get_user_order_id = Order::where('user_id',  $user -> id) ->where('status', '!=', 0);
+        
+        $history_paid =  $get_user_order_id ->get() ->sum('total_price');
+        $history_count =  $get_user_order_id ->where('status', '!=', 0) ->count();
+        $user_orders =  $get_user_order_id ->where('status', '!=', 0) ->get();
+      
+        $books = [];
+        foreach ($user_orders as $get_item) {
+          $book = Detail_order::where('order_id', $get_item -> id) ->sum('quantity');
+          array_push($books, $book);
+        }
+        
+        $books_count = array_sum($books);
+     
+      //   dump($cart);
+      // dump($total_pay);
+      
+      // dump($history_count);
+      
+      // dump($history_paid);
+      // dd($books_count);
+
+        return view('dashboard', compact('cart', 'total_pay', 'history_count', 'history_paid', 'books_count'));
+      }
+    
+      public function admin() {
+    
+        return view('admin/adminHome');
+    
+      }
 }
--- resources/views/layouts/headers/cards.blade.php
+++ resources/views/layouts/headers/cards.blade.php
@@ -13,7 +13,7 @@
               <div class="row">
                 <div class="col">
                   <h5 class="card-title text-uppercase text-muted mb-0">Your Cart</h5>
-                  <span class="h2 font-weight-bold mb-0">0 pcs</span>
+                  <span class="h2 font-weight-bold mb-0">{{ $cart }} pcs</span>
                 </div>
                 <div class="col-auto">
                   <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
@@ -22,7 +22,7 @@
                 </div>
               </div>
               <p class="mt-3 mb-0 text-muted text-sm">
-                <span class="text-success mr-2">Rp 10.303 ,-</span>
+                <span class="text-success mr-2">Rp. {{ number_format($total_pay) }} ,-</span>
                 <span class="text-nowrap">Checkout now...</span>
               </p>
             </div>
@@ -34,7 +34,7 @@
               <div class="row">
                 <div class="col">
                   <h5 class="card-title text-uppercase text-muted mb-0">Orders History</h5>
-                  <span class="h2 font-weight-bold mb-0">2 Transactions</span>
+                  <span class="h2 font-weight-bold mb-0">Rp. {{ $history_count }} Transactions</span>
                 </div>
                 <div class="col-auto">
                   <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
@@ -43,7 +43,7 @@
                 </div>
               </div>
               <p class="mt-3 mb-0 text-muted text-sm">
-                <span class="text-success mr-2"> Rp 120.292</span>
+                <span class="text-success mr-2">Rp. {{ number_format($history_paid) }} ,-</span>
                 <span class="text-nowrap">Since last week</span>
               </p>
             </div>
@@ -55,7 +55,7 @@
               <div class="row">
                 <div class="col">
                   <h5 class="card-title text-uppercase text-muted mb-0">Books Collection</h5>
-                  <span class="h2 font-weight-bold mb-0">4 pcs</span>
+                  <span class="h2 font-weight-bold mb-0">{{ $books_count }} pcs</span>
                 </div>
                 <div class="col-auto">
                   <div class="icon icon-shape bg-yellow text-white rounded-circle shadow">
@@ -64,7 +64,7 @@
                 </div>
               </div>
               <p class="mt-3 mb-0 text-muted text-sm">
-                <span class="text-primary mr-2"> Rp. 120.000,-</span>
+                <span class="text-primary mr-2">Rp. {{ number_format($history_paid) }} ,-</span>
                 <span class="text-nowrap">This Month</span>
               </p>
             </div>
