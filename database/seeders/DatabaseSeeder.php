4bf3441601b15a3f262ab06fd11b74dd9b360d77
ricky03knowhere <goose73thecat@gmail.com>
2021-06-23 17:47:31 +0700

    edit database  seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UsersTableSeeder::class]);
+        $this->call([CreateUsersSeeder::class]);
+        $this->call([CreateComicsSeeder::class]);
     }
 }
